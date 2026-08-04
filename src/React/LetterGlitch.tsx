bbec6810b83f3ba819a363ebed52205ec09c9190
studionomadid <studionomad.id@gmail.com>
2026-08-04 09:27:31 +0700

    fix: resolve NodeJS timeout type

--- src/React/LetterGlitch.tsx
+++ src/React/LetterGlitch.tsx
@@ -257,7 +257,7 @@ const LetterGlitch = ({
     resizeCanvas();
     animate();
 
-    let resizeTimeout: NodeJS.Timeout;
+    let resizeTimeout: ReturnType<typeof setTimeout>;
 
     const handleResize = () => {
       clearTimeout(resizeTimeout);
